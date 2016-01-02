159836521707b32d78555537684b27cadff23bbd
softprops <d.tangren@gmail.com>
2016-01-02 12:09:15 -0500

    expect a slash

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use hyperlocal::{DomainUrl, SocketConnector};
 
 fn main() {
     let client = hyper::Client::with_connector(hyperlocal::SocketConnector);
-    let mut res = client.get(DomainUrl::new("/var/run/docker.sock", "info")).send().unwrap();
+    let mut res = client.get(DomainUrl::new("/var/run/docker.sock", "/info")).send().unwrap();
     std::io::copy(&mut res, &mut std::io::stdout()).unwrap();
 }
