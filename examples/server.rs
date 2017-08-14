96cc0d058fe7a42974fc7d48c79bad2de0c191df
softprops <d.tangren@gmail.com>
2017-08-14 00:13:58 -0400

    cant cargo publish with dev dependency on service-fn

--- examples/server.rs
+++ examples/server.rs
@@ -2,27 +2,34 @@ extern crate hyper;
 extern crate hyperlocal;
 extern crate futures;
 extern crate tokio_service;
-extern crate service_fn;
 
-use hyper::{Result, Response};
+use futures::future::FutureResult;
+use hyper::{Result, Request, Response};
 use hyper::header::{ContentType, ContentLength};
-use service_fn::service_fn;
+use tokio_service::Service;
 
 const PHRASE: &'static str = "It's a Unix system. I know this.";
 
+struct Hello;
+
+impl Service for Hello {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+    fn call(&self, _req: Request) -> Self::Future {
+        futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(PHRASE.len() as u64))
+                .with_header(ContentType::plaintext())
+                .with_body(PHRASE),
+        )
+    }
+}
+
 fn run() -> Result<()> {
     let path = "test.sock";
-    let hello = || {
-        Ok(service_fn(|_| {
-            Ok(
-                Response::<hyper::Body>::new()
-                    .with_header(ContentLength(PHRASE.len() as u64))
-                    .with_header(ContentType::plaintext())
-                    .with_body(PHRASE),
-            )
-        }))
-    };
-    let svr = hyperlocal::server::Http::new().bind(path, hello)?;
+    let svr = hyperlocal::server::Http::new().bind(path, || Ok(Hello))?;
     println!("Listening on unix://{path} with 1 thread.", path = path);
     svr.run()?;
     Ok(())
