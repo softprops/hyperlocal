8c8b7fd543f66be3f913bbda69df57f0532ff8e3
Daniel Eades <danieleades@hotmail.com>
2019-12-26 14:36:30 +0000

    put client and server extensions behind feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-#![deny(missing_debug_implementations, unreachable_pub, rust_2018_idioms, missing_docs)]
+#![deny(
+    missing_debug_implementations,
+    unreachable_pub,
+    rust_2018_idioms,
+    missing_docs
+)]
 
 //! `hyperlocal` provides [Hyper](http://github.com/hyperium/hyper) bindings
 //! for [Unix domain sockets](https://github.com/tokio-rs/tokio/tree/master/tokio-net/src/uds/).
@@ -8,10 +13,14 @@
 //! [`hyperlocal::UnixServerExt`](crate::server::UnixServerExt) docs for how to
 //! configure servers.
 
+#[cfg(feature = "client")]
 mod client;
+#[cfg(feature = "client")]
 pub use client::UnixConnector;
 
+#[cfg(feature = "server")]
 mod server;
+#[cfg(feature = "server")]
 pub use server::UnixServerExt;
 
 mod uri;
