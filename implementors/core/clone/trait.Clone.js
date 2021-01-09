9248de6d707f0e7029c8d7f9d499135c1ce9bfc4
softprops <softprops@users.noreply.github.com>
2021-01-09 12:34:53 +0000

    deploy: c2480c15de050c775702ba7d65293f06873ced35

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["hyperlocal"] = [{"text":"impl Clone for UnixConnector","synthetic":false,"types":[]}];
+implementors["hyperlocal"] = [{"text":"impl Clone for UnixConnector","synthetic":false,"types":[]},{"text":"impl Clone for Uri","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
