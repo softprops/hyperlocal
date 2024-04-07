3ff544debed5990da039be8dc221c149ca91fc7d
softprops <softprops@users.noreply.github.com>
2024-04-07 22:52:13 +0000

    deploy: adf35883be822fd088cb37012c936f9cfac6b02b

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hyperlocal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"hyperlocal/struct.Uri.html\" title=\"struct hyperlocal::Uri\">Uri</a>&gt; for Uri"]]
+"hyperlocal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"hyperlocal/struct.Uri.html\" title=\"struct hyperlocal::Uri\">Uri</a>&gt; for Uri"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hyperlocal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"hyperlocal/struct.UnixConnector.html\" title=\"struct hyperlocal::UnixConnector\">UnixConnector</a>"]]
+"hyperlocal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"hyperlocal/struct.UnixConnector.html\" title=\"struct hyperlocal::UnixConnector\">UnixConnector</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
